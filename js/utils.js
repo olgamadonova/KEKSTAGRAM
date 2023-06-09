9ea6af1b42746f822e44814011aa47c2027d9047
olgadeveloper <madonovaolga@icloud.com>
2023-06-09 14:19:26 +0700

    added amendments in utils functions

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,4 @@
-const stringCleanPattern = /[\s\\.]/ig;
-
-const isValidLength = (string, lengthValue) => typeof string === 'string' ? string.length <= lengthValue : String(string).length <= lengthValue;
+const isValidLength = (string, lengthValue) => string.length <= lengthValue;
 
 isValidLength('instagram', 10);
 
@@ -9,22 +7,11 @@ const isPalindrome = (string) => {
     return;
   }
   const normalizedString = string.toLowerCase().replaceAll(' ', '');
-  return normalizedString.split('').reverse().join('') === normalizedString;
+  return [...normalizedString].reverse().join('') === normalizedString;
 };
 
 isPalindrome('Лёша на полке клопа нашёл ');
 
-const getNumbers = (string) => {
-  const validString = typeof string === 'string' ? string : String(string);
-  const numbersArray = validString.split(' ').filter((item) => ! Number.isNaN(parseFloat(item)));
-
-  if (!numbersArray.length) {
-    return NaN;
-  }
-  const cleanedNumber = 1 * (numbersArray.map((numberInStr) =>Math.abs(parseFloat(numberInStr))).join(' ').replaceAll(stringCleanPattern, ''));
-  return cleanedNumber;
-};
-
+const getNumbers = (string) => parseInt(string.replace(/\D+/g, ''), 10);
 getNumbers('1 кефир, 0.5 батона');
-getNumbers('1.5');
 
