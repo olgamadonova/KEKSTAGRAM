598033beb229c0461fbf402f99eabac6ae5096d7
olgadeveloper <madonovaolga@icloud.com>
2023-07-18 20:39:48 +0700

    auto test passed

--- js/show-photo-preview.js
+++ js/show-photo-preview.js
@@ -1,6 +1,7 @@
 import { normalizeString } from './utils.js';
 
 const FILE_TYPES = ['gif', 'webp', 'jpeg', 'png', 'avif', 'jpg', 'svg'];
+const photoEffectPreviews = document.querySelectorAll('.effects__preview');
 
 const uploadInputElement = document.querySelector('.img-upload__input');
 const photoPreviewElement = document.querySelector('.img-upload__preview img');
@@ -13,6 +14,10 @@ const showPhotoPreview = () => {
 
   if (isMatching) {
     photoPreviewElement.src = URL.createObjectURL(file);
+
+    photoEffectPreviews.forEach((preview) => {
+      preview.style.backgroundImage = `url(${photoPreviewElement.src})`;
+    });
   }
 };
 
